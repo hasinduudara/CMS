f4d0062c4113dea071ba1c61f519a363fdbfec09
hasinduudara <hasindutechschool@gmail.com>
2025-06-11 13:46:00 +0530

    update database

--- db/schema.sql
+++ db/schema.sql
@@ -2,17 +2,17 @@ CREATE DATABASE cms_db;
 USE cms_db;
 
 CREATE TABLE users (
-                       id VARCHAR PRIMARY KEY AUTO_INCREMENT,
-                       username VARCHAR(50) UNIQUE,
-                       password VARCHAR(255),
-                       role ENUM('EMPLOYEE', 'ADMIN')
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       username VARCHAR(50) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       role ENUM('EMPLOYEE', 'ADMIN') NOT NULL
 );
 
 CREATE TABLE complaints (
-                            id VARCHAR PRIMARY KEY AUTO_INCREMENT,
-                            user_id INT,
-                            title VARCHAR(100),
-                            description TEXT,
+                            id INT PRIMARY KEY AUTO_INCREMENT,
+                            user_id INT NOT NULL,
+                            title VARCHAR(100) NOT NULL,
+                            description TEXT NOT NULL,
                             status ENUM('PENDING', 'IN_PROGRESS', 'RESOLVED') DEFAULT 'PENDING',
                             remark TEXT,
                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
