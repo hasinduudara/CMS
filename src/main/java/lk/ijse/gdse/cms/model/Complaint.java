811e2d9f95cc8def20a9b2d7952be24a07ef8630
hasinduudara <hasindutechschool@gmail.com>
2025-06-11 13:41:51 +0530

    create complaint model

---
+++ src/main/java/lk/ijse/gdse/cms/model/Complaint.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.cms.model;
+
+import java.sql.Timestamp;
+
+public class Complaint {
+    private String id;
+    private String userId;
+    private String title;
+    private String description;
+    private String status;
+    private String remark;
+    private Timestamp complaintTime;
+}
